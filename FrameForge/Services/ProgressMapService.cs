ab3b94506abbf0fe7e7d708cdf42197c97ce1d52
nazarsokal <93711302+nazarsokal@users.noreply.github.com>
2025-04-07 21:27:14 +0200

    Implementation of two methods
    1) EnrolOnLevel()
    2) EnrollOffLevel()

--- FrameForge/Services/ProgressMapService.cs
+++ FrameForge/Services/ProgressMapService.cs
@@ -5,14 +5,27 @@ namespace Services;
 
 public class ProgressMapService : IProgressMapService
 {
+    private readonly FrameForgeDbContext _dbContext;
+
+    public ProgressMapService(FrameForgeDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
     public void EnrolOnLevel(Student studentEnrolled, string levelTopicName)
     {
-        throw new NotImplementedException();
+        var levelEnrolled = new LevelsEnrolled() {StudentId = studentEnrolled.StudentId, State = "In Progress", LevelTopicName = levelTopicName, MoneyReward = 0.0, StarsReward = 0};
+        
+        _dbContext.LevelsEnrolled.Add(levelEnrolled);
+        _dbContext.SaveChanges();
     }
 
     public void EnrolOffLevel(Student studentEnrolled, string levelTopicName)
     {
-        throw new NotImplementedException();
+        var levelToEnrollOff = _dbContext.LevelsEnrolled.FirstOrDefault(x => x.StudentId == studentEnrolled.StudentId && x.LevelTopicName == levelTopicName);
+        if(levelToEnrollOff == null) throw new NullReferenceException();
+        
+        _dbContext.LevelsEnrolled.Remove(levelToEnrollOff);
+        _dbContext.SaveChanges();
     }
 
     public void CompleteOnLevel(Student studentEnrolled, string levelTopicName)
