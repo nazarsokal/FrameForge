072e7dc5518092629d8b33480d4d016200c9b341
Olexandr Herhel <sfhk3832@gmail.com>
2025-04-07 20:24:22 +0300

    Leader Board Service

---
+++ FrameForge/Services/LeaderboardService.cs
@@ -0,0 +1,26 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    internal class LeaderboardService
+    {
+        private readonly FrameForgeDbContext _dbContext;
+        public LeaderboardService(FrameForgeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public List<Student> GetAllSorted()
+        {
+            List<Student> all = _dbContext.Students.ToList();
+            all.Sort((x, y) => y.StarsAmount.CompareTo(x.StarsAmount));
+            return all;
+
+        }
+
+    }
+}
