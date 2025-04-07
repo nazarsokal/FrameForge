f268dc32b07a3cfe4162eed67bf0515456bd1f0d
nazarsokal <93711302+nazarsokal@users.noreply.github.com>
2025-04-07 21:19:33 +0200

    Serive Contract Created
    Created a service contract for ProgressMapService and created a service implementing this contract

---
+++ FrameForge/ServiceContracts/IProgressMapService.cs
@@ -0,0 +1,10 @@
+using Entities;
+
+namespace ServiceContracts;
+
+public interface IProgressMapService
+{
+    public void EnrolOnLevel(Student studentEnrolled, string levelTopicName);
+    public void EnrolOffLevel(Student studentEnrolled, string levelTopicName);
+    public void CompleteOnLevel(Student studentEnrolled, string levelTopicName);
+}
---
+++ FrameForge/Services/ProgressMapService.cs
@@ -0,0 +1,22 @@
+using Entities;
+using ServiceContracts;
+
+namespace Services;
+
+public class ProgressMapService : IProgressMapService
+{
+    public void EnrolOnLevel(Student studentEnrolled, string levelTopicName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EnrolOffLevel(Student studentEnrolled, string levelTopicName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CompleteOnLevel(Student studentEnrolled, string levelTopicName)
+    {
+        throw new NotImplementedException();
+    }
+}
