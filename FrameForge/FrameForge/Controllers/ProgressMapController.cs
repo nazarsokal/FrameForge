2ad4f65612addb829aae4213e1e62fc2c7650971
nazarsokal <93711302+nazarsokal@users.noreply.github.com>
2025-04-08 20:41:02 +0200

    Controller Logic For ProgressMap
    Added enrolling functionality, so users can enrol on CG level

--- FrameForge/FrameForge/Controllers/ProgressMapController.cs
+++ FrameForge/FrameForge/Controllers/ProgressMapController.cs
@@ -1,18 +1,57 @@
+using System.Text.Json;
+using Entities;
 using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
 
 namespace FrameForge.Controllers;
 
 public class ProgressMapController : Controller
 {
+    private readonly IProgressMapService _service;
+    private List<LevelsEnrolled>? userLevelsEnrolled;
+    private Student? student;
+
+    public ProgressMapController(IProgressMapService service)
+    {
+        _service = service;
+    }
     [Route("[action]")]
     public IActionResult Map()
     {
+        var student = GetStudentFromSession();
+        userLevelsEnrolled = _service.GetUsersEnrolledLevels(student);
+        ViewBag.CompletedLevels = userLevelsEnrolled;
+        
         return View();
     }
 
-    [Route("[action]{levelName}")]
+    [Route("[action]")]
     public IActionResult ViewLevel(string levelName)
     {
+        var student = GetStudentFromSession();
+        
+        if (userLevelsEnrolled == null)
+        {
+            userLevelsEnrolled = new List<LevelsEnrolled>(); 
+        }
+        
+        var isLevelEnrolled =
+            userLevelsEnrolled.Any(l => l.LevelTopicName == levelName && student.StudentId == l.StudentId);
+        if (!isLevelEnrolled)
+        {
+            _service.EnrolOnLevel(student, levelName);    
+            return View(levelName);
+        }
+        
         return View(levelName);
+    }
+    
+    private Student GetStudentFromSession()
+    {
+        var studentJson = HttpContext.Session.GetString("Student");
+        if (studentJson != null) student = JsonSerializer.Deserialize<Student>(studentJson);
+        if(student == null) throw new NullReferenceException("Student is null");
+        
+        return student;
     }
 }
