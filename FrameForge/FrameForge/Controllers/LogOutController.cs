6d17f774d8bc1297f9ba9a28e08cfc415c9911a6
Olexandr Herhel <sfhk3832@gmail.com>
2025-04-11 18:42:40 +0300

    LogOut Controller

---
+++ FrameForge/FrameForge/Controllers/LogOutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrameForge.Controllers
+{
+    public class LogOutController : Controller
+    {
+        [Route("[action]")]
+        public IActionResult logOut()
+        {
+            HttpContext.Session.Remove("Student");
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
