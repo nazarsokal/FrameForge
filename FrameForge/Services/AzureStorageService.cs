01f63fe7491c8dd5d44b39f004265d9d4ad1aa17
nazarsokal <93711302+nazarsokal@users.noreply.github.com>
2025-04-24 11:30:21 +0200

    Implemented Image Uploading

--- FrameForge/Services/AzureStorageService.cs
+++ FrameForge/Services/AzureStorageService.cs
@@ -6,34 +6,44 @@ namespace Services;
 public class AzureStorageService : IAzureStorageService
 {
     private const string SasToken =
-        "https://frameforge.file.core.windows.net/?sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-05-31T15:32:24Z&st=2025-04-24T07:32:24Z&spr=https,http&sig=CaeVgMB667YopUKVMTkGOrv3QfzgXL9SkEm1w9I34AM%3D";
+        "BlobEndpoint=https://frameforge.blob.core.windows.net/;QueueEndpoint=https://frameforge.queue.core.windows.net/;FileEndpoint=https://frameforge.file.core.windows.net/;TableEndpoint=https://frameforge.table.core.windows.net/;SharedAccessSignature=sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-05-31T17:11:47Z&st=2025-04-24T09:11:47Z&spr=https,http&sig=K4KyYjzpW7jCTRrDvYoM%2FZksW58hIL3wGUzuWw1Zc70%3D";
+
+    private readonly ShareClient _shareClient;
 
     public AzureStorageService()
     {
-        
+        // Initialize ShareClient using the SAS token connection string
+        _shareClient = new ShareClient(SasToken, "frameforgefilestorage");
     }
-    
+
     public async Task UploadUserPhoto(string photoPath, Guid userId)
     {
-        ShareClient share = new ShareClient(new Uri(SasToken));
+        try
+        {
+            ShareDirectoryClient rootDir = _shareClient.GetRootDirectoryClient();
 
-        // 🔹 Get or create the target folder
-        ShareDirectoryClient rootDir = share.GetRootDirectoryClient();
-        ShareDirectoryClient folder = rootDir.GetSubdirectoryClient("UserImages");
-        
-        // await folder.CreateIfNotExistsAsync();
-        
-        string filePath = userId.ToString() + ".jpg";
-        ShareFileClient fileClient = folder.GetFileClient(filePath);
-        
-        FileInfo fileInfo = new FileInfo(filePath);
-        await fileClient.CreateAsync(fileInfo.Length);
-        
-        using FileStream stream = File.OpenRead(filePath);
-        await fileClient.UploadRangeAsync(
-            new Azure.HttpRange(0, fileInfo.Length),
-            stream
-        );
+            ShareDirectoryClient folder = rootDir.GetSubdirectoryClient("UserImages");
+
+            string targetFileName = $"{userId}.jpg";
+            
+            ShareFileClient fileClient = folder.GetFileClient(targetFileName);
+            
+            FileInfo fileInfo = new FileInfo(photoPath);
+            
+            await fileClient.CreateAsync(fileInfo.Length);
+            
+            using FileStream stream = File.OpenRead(photoPath);
+            await fileClient.UploadRangeAsync(
+                new Azure.HttpRange(0, fileInfo.Length),
+                stream
+            );
+        }
+        catch (Exception ex)
+        {
+            // Log the exception for debugging
+            Console.WriteLine($"Error uploading photo: {ex.Message}");
+            throw; // Re-throw the exception to handle it upstream if needed
+        }
     }
 
     public Task<string> GetUserPhoto(Guid userId)
--- FrameForge/ServicesTest/AzureServiceTest.cs
+++ FrameForge/ServicesTest/AzureServiceTest.cs
@@ -12,12 +12,12 @@ public class AzureServiceTest
         _azureStorageService = new AzureStorageService();
     }
     [Fact]
-    public void TestAzureService_LoadUserPhoto()
+    public async Task TestAzureService_LoadUserPhoto()
     {
         string photoPath =
             @"/Users/asokalch/Documents/GitHub/FrameForge/FrameForge/FrameForge/wwwroot/images/users/e4c52f39-b100-4d25-ac78-067f23667a1f.jpg";
         
-        _azureStorageService.UploadUserPhoto(photoPath, Guid.NewGuid());
+        await _azureStorageService.UploadUserPhoto(photoPath, Guid.NewGuid());
         
         Assert.NotNull(photoPath);
     }
