44d1c34f3e2d596c076a2b3241bc170bce8d8aa1
NazarNovosilets <nazar.novosilets100@gmail.com>
2025-04-24 13:04:51 +0300

    add modal window
    add window for introduction lvl

---
+++ FrameForge/FrameForge/wwwroot/js/mapPage_js/mapPage.js
@@ -0,0 +1,33 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    
+    //Відкривання модального вікна при натисканні на осрови
+    const islands = document.querySelectorAll('.firstIsland, .secondIsland, .thirdIsland, .fourthIsland');
+    const sidebar = document.querySelector('#modalWindow');
+    const islandImages = document.querySelectorAll('.islandFirstLvl, .islandSecondLvl, .islandThirdLvl, .islandFourthLvl');
+
+    if (islandImages && sidebar) {
+        islandImages.forEach(image => {
+            image.addEventListener('click', (e) => {
+                e.preventDefault();
+                sidebar.classList.add('active');
+                islands.forEach(island => {
+                    const islandRect = island.getBoundingClientRect();
+                    if (islandRect.right > window.innerWidth - 300) {
+                        island.classList.add('shifted');
+                    }
+                });
+            });
+        });
+    }
+
+    //Закриття модального вікна
+    window.closeSidebar = function() {
+        if (sidebar) {
+            sidebar.classList.remove('active');
+            // Повертаємо острови на місце
+            islands.forEach(island => {
+                island.classList.remove('shifted');
+            });
+        }
+    };
+});
