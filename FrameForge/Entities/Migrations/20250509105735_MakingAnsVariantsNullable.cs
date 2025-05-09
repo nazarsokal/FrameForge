9957b16e4159c3bfe3047bc233dc9d7c0f7baee6
Olexandr Herhel <sfhk3832@gmail.com>
2025-05-09 21:22:24 +0300

    Progress Bar
    Fixed progress bar logic, added styles for three new pages

---
+++ FrameForge/Entities/Migrations/20250509105735_MakingAnsVariantsNullable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakingAnsVariantsNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Student",
+                keyColumn: "StudentId",
+                keyValue: new Guid("537c815e-d0d6-4b66-bfd1-bfcc735dd31f"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnswerVariants",
+                table: "Tests",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "StudentId", "Email", "GoogleId", "MoneyAmount", "Password", "Picture", "StarsAmount", "Username" },
+                values: new object[] { new Guid("a20a1008-a76a-4a19-8edc-ff7c5ba60650"), "TestUser@test.com", null, 100.45, "TestPassword", null, 0, "TestUserName" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Student",
+                keyColumn: "StudentId",
+                keyValue: new Guid("a20a1008-a76a-4a19-8edc-ff7c5ba60650"));
+
+            migrationBuilder.UpdateData(
+                table: "Tests",
+                keyColumn: "AnswerVariants",
+                keyValue: null,
+                column: "AnswerVariants",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnswerVariants",
+                table: "Tests",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "StudentId", "Email", "GoogleId", "MoneyAmount", "Password", "Picture", "StarsAmount", "Username" },
+                values: new object[] { new Guid("537c815e-d0d6-4b66-bfd1-bfcc735dd31f"), "TestUser@test.com", null, 100.45, "TestPassword", null, 0, "TestUserName" });
+        }
+    }
+}
--- FrameForge/FrameForge/Controllers/TestsController.cs
+++ FrameForge/FrameForge/Controllers/TestsController.cs
@@ -1,23 +1,77 @@
 ﻿using Entities;
 using Microsoft.AspNetCore.Mvc;
 using Services;
+using System.Text.Json;
 
-namespace FrameForge.Controllers;
-public class TestsController : Controller
+namespace FrameForge.Controllers
 {
-    TestsServise _testsServise;
-    List<Test> _tests = null;
-    public TestsController(TestsServise testsServise)
+    public class TestsController : Controller
     {
-        _testsServise = testsServise;
-    }
-    // GET
-    [Route("[action]")]
+        private readonly TestsServise _testsServise;
 
-    public async Task<IActionResult> StartTesting( string title = "intro")
-    {
-        _tests = await _testsServise.getAllTests(title);
-        List<TestQuestionModel> tests = _testsServise.convertFromDto(_tests);
-        return View(tests[0]);
+        public TestsController(TestsServise testsServise)
+        {
+            _testsServise = testsServise;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> StartTesting(string title = "intro")
+        {
+            List<Test> tests = await _testsServise.getAllTests(title);
+            List<TestQuestionModel> testModels = _testsServise.convertFromDto(tests);
+
+            // Зберігаємо список і лічильник у Session
+            HttpContext.Session.SetString("Tests", JsonSerializer.Serialize(testModels));
+            HttpContext.Session.SetInt32("Counter", 0);
+            HttpContext.Session.SetInt32("Count", testModels.Count);
+            
+            ViewBag.Counter = 0;
+            ViewBag.Count = testModels.Count;
+            return View(testModels[0]);
+        }
+        
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult NextTest(int counter)
+        {
+            var testsJson = HttpContext.Session.GetString("Tests");
+            var tests = JsonSerializer.Deserialize<List<TestQuestionModel>>(testsJson);
+    
+            if (tests == null || counter >= tests.Count)
+                return RedirectToAction("Index", "Home");
+
+            // Оновлюємо каунтер в сесії
+            HttpContext.Session.SetInt32("Counter", counter);
+            ViewBag.Counter = counter;
+            ViewBag.Count = tests.Count;
+            
+            return View("StartTesting", tests[counter]);
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult CheckAnswer(int counter, string answer)
+        {
+            var testsJson = HttpContext.Session.GetString("Tests");
+            var tests = JsonSerializer.Deserialize<List<TestQuestionModel>>(testsJson);
+            
+            var currentTest = tests[counter];
+            bool isCorrect = answer == currentTest.Answer;
+            
+            // Оновлюємо каунтер в сесії
+            counter++;
+            HttpContext.Session.SetInt32("Counter", counter);
+            ViewBag.Counter = counter;
+            ViewBag.Count = tests.Count;
+            
+            if (isCorrect)
+            {
+                return View("TestAnsCorrect", currentTest);
+            }
+            else
+            {
+                return View("TestAnsIncorrect", currentTest);
+            }
+        }
     }
 }
