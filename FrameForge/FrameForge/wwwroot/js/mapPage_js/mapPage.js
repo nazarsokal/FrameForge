e02a3be91343832175939e227f6b227594c5e921
NazarNovosilets <nazar.novosilets100@gmail.com>
2025-05-10 20:03:11 +0300

    fix moved of island

--- FrameForge/FrameForge/wwwroot/js/mapPage_js/mapPage.js
+++ FrameForge/FrameForge/wwwroot/js/mapPage_js/mapPage.js
@@ -82,7 +82,7 @@
                 //Зміщення островів
                 islands.forEach(island => {
                     const islandRect = island.getBoundingClientRect();
-                    if (islandRect.right > window.innerWidth - 300) {
+                    if (islandRect.right > window.innerWidth - 320) {
                         island.classList.add('shifted');
                     }
                 });
