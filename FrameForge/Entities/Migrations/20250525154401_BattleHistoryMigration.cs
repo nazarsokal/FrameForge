b900f8c7bc58ab332c45a31ee890f992a469037c
Olexandr Herhel <sfhk3832@gmail.com>
2025-05-26 00:07:03 +0300

    batttle history migration

---
+++ FrameForge/Entities/Migrations/20250525154401_BattleHistoryMigration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class BattleHistoryMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Student",
+                keyColumn: "StudentId",
+                keyValue: new Guid("c4889752-3472-4be1-987a-e08f446c5da5"));
+
+            migrationBuilder.CreateTable(
+                name: "BattleHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Player1Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Player2Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    WinnerId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BattleHistory", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "StudentId", "Email", "GoogleId", "MoneyAmount", "Password", "Picture", "StarsAmount", "Username" },
+                values: new object[] { new Guid("2170211e-dbd7-4653-a614-36736198cd10"), "TestUser@test.com", null, 100.45, "TestPassword", null, 0, "TestUserName" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BattleHistory");
+
+            migrationBuilder.DeleteData(
+                table: "Student",
+                keyColumn: "StudentId",
+                keyValue: new Guid("2170211e-dbd7-4653-a614-36736198cd10"));
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "StudentId", "Email", "GoogleId", "MoneyAmount", "Password", "Picture", "StarsAmount", "Username" },
+                values: new object[] { new Guid("c4889752-3472-4be1-987a-e08f446c5da5"), "TestUser@test.com", null, 100.45, "TestPassword", null, 0, "TestUserName" });
+        }
+    }
+}
