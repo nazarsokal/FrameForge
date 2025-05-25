5944db7eed81ee504055933c2260f9786266c639
Olexandr Herhel <sfhk3832@gmail.com>
2025-05-26 00:39:50 +0300

    Update BattleSingleton.cs

--- FrameForge/Services/Hubs/BattleSingleton.cs
+++ FrameForge/Services/Hubs/BattleSingleton.cs
@@ -4,7 +4,25 @@ public class BattleSingleton
 {
    
     private readonly Dictionary<Guid, bool> _readyClients = new();
+    private readonly Queue<Guid> RoomsWrittenToHistory = new();
     private readonly object _lock = new();
+
+    public bool CheckRoom(Guid roomId)
+    {
+        lock (_lock)
+        {
+            if (RoomsWrittenToHistory.Contains(roomId)) return false;
+            else return true;
+        }
+    }
+    public void AddRoomToHistory(Guid roomId)
+    {
+        lock (_lock)
+        {
+            RoomsWrittenToHistory.Enqueue(roomId);
+            if (RoomsWrittenToHistory.Count > 10) RoomsWrittenToHistory.Dequeue();
+        }
+    }
     public void Add(Guid connectionId)
     {
         lock (_lock)
