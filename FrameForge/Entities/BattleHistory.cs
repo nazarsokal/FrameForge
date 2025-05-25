2d500a984f6001845161914b421147b8b45b7d75
Olexandr Herhel <sfhk3832@gmail.com>
2025-05-26 00:07:37 +0300

    Battle history model

--- FrameForge/Entities/BattleHistory.cs
+++ FrameForge/Entities/BattleHistory.cs
@@ -1,6 +1,15 @@
-﻿namespace Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities;
 
 public class BattleHistory
 {
+    [Key]
+    public Guid Id { get; set; }
+    public Guid Player1Id { get; set; }
+    public Guid Player2Id { get; set; }
+    public DateTime EndTime {get; set;}
+
+    public Guid WinnerId { get; set; }
     
 }
